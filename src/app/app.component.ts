fb9a6cabc71d6448a7c64a2b22b20c42d3551834
Selvia-Emad <Selviaemad58@gmail.com>
2025-08-21 03:55:22 +0300

    navsmallbanner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,11 +16,14 @@ import { TrustedByComponent } from "./sections/trusted-by/trusted-by.component";
 import { FooterComponent } from "./pages/components/footer/footer.component";
 import { ProductsComponent } from "./pages/components/products/products.component";
 import { Component } from "@angular/core";
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+
 
 
 @Component({
   selector: 'app-root',
   imports: [
+    CarouselModule ,
     RouterOutlet,
     HeaderComponent,
     HeroSectionComponent,
@@ -39,5 +42,28 @@ TestimonialsCarouselComponent , TrustedByComponent , FooterComponent,ProductsCom
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'home-rose-site';
+ customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
 }
--- src/app/pages/components/header/header.component.ts
+++ src/app/pages/components/header/header.component.ts
@@ -1,11 +1,31 @@
 
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // مهمة لإظهار ngIf و ngFor
+// import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common'; // مهمة لإظهار ngIf و ngFor
 
+
+// @Component({
+//   selector: 'app-header',
+//   standalone: true, // تأكد من أن هذا صحيح
+//   imports: [
+//     CommonModule,
+//   ],
+//   templateUrl: './header.component.html',
+//   styleUrls: ['./header.component.scss']
+// })
+// export class HeaderComponent {
+
+
+// }
+
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
+import { ButtonModule } from "primeng/button";
+import { InputTextModule } from 'primeng/inputtext';
+import { DialogModule } from 'primeng/dialog'; // ✅ هذا هو الموديول الصحيح
 
 @Component({
   selector: 'app-header',
-  standalone: true, // تأكد من أن هذا صحيح
   imports: [
     CommonModule,
   ],
@@ -13,6 +33,9 @@ import { CommonModule } from '@angular/common'; // مهمة لإظهار ngIf و
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
- 
+  sidebarVisible: boolean = false;
 
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
 }
