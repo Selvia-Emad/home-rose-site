8d1c8417935986c30d10e1cce128771907d671a9
Selvia-Emad <Selviaemad58@gmail.com>
2025-08-21 12:32:17 +0300

    6th commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,28 +44,28 @@ import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
- customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: true,
-    navSpeed: 700,
-    navText: ['', ''],
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 4
-      }
-    },
-    nav: true
-  }
+//  customOptions: OwlOptions = {
+//     loop: true,
+//     mouseDrag: false,
+//     touchDrag: false,
+//     pullDrag: false,
+//     dots: true,
+//     navSpeed: 700,
+//     navText: ['', ''],
+//     responsive: {
+//       0: {
+//         items: 1
+//       },
+//       400: {
+//         items: 2
+//       },
+//       740: {
+//         items: 3
+//       },
+//       940: {
+//         items: 4
+//       }
+//     },
+//     nav: true
+//   }
 }
