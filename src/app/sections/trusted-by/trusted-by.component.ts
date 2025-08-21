fcefc058d12e4ddac7b3f70314c609078da04a41
Selvia-Emad <Selviaemad58@gmail.com>
2025-08-21 12:28:13 +0300

    5th commit

--- src/app/sections/trusted-by/trusted-by.component.ts
+++ src/app/sections/trusted-by/trusted-by.component.ts
@@ -1,55 +1,11 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-trusted-by',
-//   imports: [],
-//   templateUrl: './trusted-by.component.html',
-//   styleUrl: './trusted-by.component.scss'
-// })
-// export class TrustedByComponent {
-
-// }
-
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { OwlOptions, CarouselModule } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-trusted-by',
+  imports: [],
   templateUrl: './trusted-by.component.html',
-  styleUrl: './trusted-by.component.scss',
-  standalone: true,
-  imports: [CommonModule, CarouselModule]
+  styleUrl: './trusted-by.component.scss'
 })
 export class TrustedByComponent {
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: false,
-    navSpeed: 700,
-    navText: ['', ''],
-    responsive: {
-      0: {
-        items: 2
-      },
-      768: {
-        items: 4
-      },
-      992: {
-        items: 6
-      }
-    },
-    nav: false
-  };
 
-  logos = [
-    { src: 'assets/images/logo-1.png', alt: 'Company Logo 1' },
-    { src: 'assets/images/logo-2.png', alt: 'Company Logo 2' },
-    { src: 'assets/images/logo-3.png', alt: 'Company Logo 3' },
-    { src: 'assets/images/logo-4.png', alt: 'Company Logo 4' },
-    { src: 'assets/images/logo-5.png', alt: 'Company Logo 5' },
-    { src: 'assets/images/logo-6.png', alt: 'Company Logo 6' }
-  ];
 }
